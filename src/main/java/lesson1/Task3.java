9a26e84a18211dd82810ef6a4941342783e981ac
vdmytriukhin <vdmytriukhin@foundationmedicine.com>
2019-07-01 18:55:56 +0300

    initial commit
    readme file added
    
    Added advansed tasks for lesson1
    
    Renamed folder
    
    added lesson2 task
    
    task1 gotovo
    
    task2 gotovo
    
    vse sdelal
    
    peredelal

--- src/main/java/lesson1/Task3.java
+++ src/main/java/lesson1/Task3.java
@@ -3,19 +3,26 @@ package lesson1;
 /* Мое имя
 Вывести на экран свое имя 5 строк по 10 раз (через пробел).
 System.out.println, System.out.print можно использовать только по одному разу
-для этого нужно воспользоваться циклом do () while
+для этого нужно воспользоваться циклом while
 */
 
-public class Task4 {
+public class Task3 {
+
     public static void main(String[] args) {
         String name = "Eugene";
         int i = 0;
-        do{System.out.println();i++;{
-            int j = 0; do {System.out.print(name + " ");j++;}
-            while (j<10);}} while (i<5 );
+        while (i < 5)
+        {
+            int j = 0;
+            while (j < 10)
+            {
+                System.out.print(name + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
         }
+
+    }
+
 }
-
-
-
-
